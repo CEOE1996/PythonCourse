245dd4e0dca0eb6c1436123480fbbf9661bd4f76
Carlos Enrique Orozco Estrada <corozco@imsalum.com>
2017-10-15 20:49:12 -0500

    Suggesting Words

--- Application 1 Dictionary/Dictionary.py
+++ Application 1 Dictionary/Dictionary.py
@@ -5,14 +5,29 @@ data = json.load(open("data.json"))
 
 def translate(word):
     word = word.lower()
+    sw = get_close_matches(word, data.keys())
     if word in data:
         return data[word]
-    elif len(get_close_matches(word, data.keys()) > 0:
-        return "Did you mean %s?" % get_close_matches(word, data.keys())[0]
 
+    elif len(sw) > 0:
+        yn = input("Did you mean %s?\nY = Yes, N = No: " % sw[0])
+
+        if yn.upper() == 'Y':
+            return data[sw[0]]
+        elif yn.upper() == 'N':
+            return "The word doesn't exists"
+        else:
+            return "Wrong answer"
     else:
         return "The word doesn't exists"
 
 word = input("Enter your Word: ")
 
-print(translate(word))
+r = translate(word)
+
+if type(r) == list:
+    for w in r
+        print(w)
+else:
+    print(r)
+#print(type(translate(word)))
